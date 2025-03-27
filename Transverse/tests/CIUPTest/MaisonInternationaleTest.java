a5a8d1eaf7605e9ba982f49510f47b10dfafc60f
auryo-B <129997399+auryo-B@users.noreply.github.com>
2025-03-27 12:08:27 +0100

    WIP

--- Transverse/tests/CIUPTest/MaisonInternationaleTest.java
+++ Transverse/tests/CIUPTest/MaisonInternationaleTest.java
@@ -1,5 +1,17 @@
 package CIUPTest;
 
-public class MaisonInternationaleTest {
+import CIUP.MaisonInternationale;
 
+public class MaisonInternationaleTest {
+	
+	private MaisonInternationale maisonInter;
+	
+	public static void main(String arg[]) {
+		MaisonInternationaleTest test = new MaisonInternationaleTest();
+		
+		
+		System.out.println("All tests passed.");
+	}
+	
+	
 }
