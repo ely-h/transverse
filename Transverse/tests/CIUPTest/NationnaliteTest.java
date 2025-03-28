12ae62cb0c3bb41cb151118ebf7992caab6573a3
Rania Hassine <rania.hassine@universite-paris-saclay.fr>
2025-03-28 12:20:03 +0100

    Implementation of the addEtudiant_EtudiantNull_ThrowsIllegalArgumentException unit test

--- Transverse/tests/CIUPTest/NationnaliteTest.java
+++ Transverse/tests/CIUPTest/NationnaliteTest.java
@@ -6,6 +6,22 @@ public class NationnaliteTest {
 	static void addEtudiant_EtudiantNonNull_AjouteEtudiantAPartirDuConstructeur() {
 		Nationnalite nationnaliteTest=new Nationnalite("test");
 		Etudiant etudiantTest=new Etudiant("","","",0,nationnaliteTest);
+		assert (nationnaliteTest.getSesEtudiants().contains(etudiantTest));
+		System.out.println("addEtudiant_EtudiantNonNull_AjouteEtudiantAPartirDuConstructeur passed");
+	}
+	
+	static void addEtudiant_EtudiantNull_ThrowsIllegalArgumentException () {
+		try {
+			Nationnalite nationnaliteTest=new Nationnalite("test");
+			nationnaliteTest.AddEtudiant(null);
+			assert false : "no exception thrown";
+		}
+		catch(IllegalArgumentException e) {
+			System.out.println("addEtudiant_EtudiantNull_ThrowsIllegalArgumentException passed");
+		}
+		catch(Exception error) {
+			assert false : "Expected IllegalArgumentException";
+		}
 	}
 
 }
