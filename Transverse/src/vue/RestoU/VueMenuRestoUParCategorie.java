d093dfb02d26d08b2fb172267507a044eae6e983
Rania Hassine <rania.hassine@universite-paris-saclay.fr>
2025-05-07 16:43:20 +0200

    creation dossier img

--- Transverse/src/vue/RestoU/VueMenuRestoUParCategorie.java
+++ Transverse/src/vue/RestoU/VueMenuRestoUParCategorie.java
@@ -25,9 +25,9 @@ public class VueMenuRestoUParCategorie extends JPanel {
 
 	public static void main(String[] args) {
 		//BufferedImage monImg=ImageIO.read(new File("tarte_citron_meringuee.webp"));
-		VueMenuRestoUParCategorie vueTest=new VueMenuRestoUParCategorie("Test","tarte_citron_meringuee.jpg");
+		VueMenuRestoUParCategorie vueTest=new VueMenuRestoUParCategorie("Test","img/pizza.jpg");
 		JFrame fenetre=new JFrame();
-		fenetre.setSize(300,300);
+		fenetre.setSize(500,800);
 		fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		fenetre.add(vueTest);
 		fenetre.setVisible(true);
