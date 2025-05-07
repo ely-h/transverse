2df02734e2786414e65ab8c742d3a9bb51dd8754
Rania Hassine <rania.hassine@universite-paris-saclay.fr>
2025-05-07 15:48:11 +0200

    Ajout initial de la vue pour une categorie du menu

---
+++ Transverse/src/vue/RestoU/VueMenuRestoUParCategorie.java
@@ -0,0 +1,37 @@
+package vue.RestoU;
+import javax.swing.*;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.image.*;
+import javax.imageio.*;
+import java.io.File;
+
+public class VueMenuRestoUParCategorie extends JPanel {
+	
+	protected JButton bouttonChoisir;
+	protected JLabel categorie;
+	protected JLabel imgCategorie;
+
+	public VueMenuRestoUParCategorie(String categorie,String imageCat){
+		
+		imgCategorie=new JLabel(new ImageIcon(imageCat));
+		this.add(imgCategorie);
+		this.categorie=new JLabel(categorie);
+		this.add(this.categorie);
+		bouttonChoisir=new JButton("Choisir");
+		this.add(bouttonChoisir);
+		this.setLayout(new GridLayout(3,1) );
+	}
+
+	public static void main(String[] args) {
+		//BufferedImage monImg=ImageIO.read(new File("tarte_citron_meringuee.webp"));
+		VueMenuRestoUParCategorie vueTest=new VueMenuRestoUParCategorie("Test","tarte_citron_meringuee.jpg");
+		JFrame fenetre=new JFrame();
+		fenetre.setSize(300,300);
+		fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		fenetre.add(vueTest);
+		fenetre.setVisible(true);
+
+	}
+
+}
