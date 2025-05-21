499849c0043eb819f80471fcff9628d2e3e764ba
auryo-B <129997399+auryo-B@users.noreply.github.com>
2025-05-21 15:43:03 +0200

    Ajout d'un main dans PanelFiltre pour voir et tester le rendu

--- Transverse/src/vue/Etudiant/Liste/PanelFiltre.java
+++ Transverse/src/vue/Etudiant/Liste/PanelFiltre.java
@@ -5,6 +5,9 @@ import java.awt.GridLayout;
 import java.util.*;
 
 import modele.Maison;
+import modele.MaisonInternationale;
+import modele.Nationnalite;
+import vue.Etudiant.Profil.Profil;
 
 public class PanelFiltre extends JPanel{
 
@@ -36,6 +39,43 @@ public class PanelFiltre extends JPanel{
 	//---------------
 	//MAIN
 	//---------------
+	public static void main (String[] arg) {
+		ArrayList<Maison> listeMaison = new ArrayList<Maison>();
+		
+		Nationnalite FR = new Nationnalite ("Française");
+		Nationnalite ES = new Nationnalite ("Espagnole");			
+		Nationnalite JP = new Nationnalite ("Japonaise");
+		Nationnalite TN = new Nationnalite ("Tunisienne");
+		Nationnalite KR = new Nationnalite ("Coréenne"); 
+		Nationnalite INT = new Nationnalite ("Internationale"); 
+	
+		Maison maisonFrance = new Maison("Maison France", FR, 48, 2, 50);
+		Maison maisonEspagne = new Maison("Maison Espagnole", ES, 40, -3, 30);
+		Maison maisonJapon = new Maison("Maison Japon", JP, 35, 139, 40);
+		Maison maisonTunisie = new Maison ("Maison Tunisie", TN, 36, 10, 35);
+		Maison maisonCoree = new Maison ("Maison Coree", KR, 37, 127, 45);
+		MaisonInternationale maisonInternationale = new MaisonInternationale ("Maison Internationale", INT, 0, 0, 100 );
+		maisonInternationale.addService("bibliotheque");
+		maisonInternationale.addService("RestoU");
+		maisonInternationale.addService("Theatre");
+		
+		listeMaison.add(maisonFrance);
+		listeMaison.add(maisonEspagne);
+		listeMaison.add(maisonJapon);
+		listeMaison.add(maisonTunisie);
+		listeMaison.add(maisonCoree);
+		listeMaison.add(maisonInternationale);
+		
+		JFrame profil = new JFrame("Profil");
+		profil.setSize(100, 300);
+		profil.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		PanelFiltre pf = new PanelFiltre(listeMaison);
+		
+		profil.add(pf);
+		profil.setVisible(true);
+		
+	}
 	
 	//---------------
 	//METHODE
