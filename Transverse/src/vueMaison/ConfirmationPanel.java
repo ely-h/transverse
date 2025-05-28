3c5f451f2674d2f8daa50d7d2489ec84b7af1d4d
Elyssa Hallou <elyssa.hallou@universite-paris-saclay.fr>
2025-05-28 16:47:59 +0200

    Update ConfirmationPanel

--- Transverse/src/vueMaison/ConfirmationPanel.java
+++ Transverse/src/vueMaison/ConfirmationPanel.java
@@ -1,5 +1,14 @@
 package vueMaison;
+import javax.swing.*;
+import java.awt.*;
+import java.util.List;
+import modele.Maison;
 
-public class ConfirmationPanel {
+public class ConfirmationPanel extends JPanel {
+	private JLabel titleLabel;
+	private JTextArea selectedHousesArea;
+	private JButton btnRetour;
+	private JButton btnValider;
+	private List<Maison> selectedMaisons;
 
 }
