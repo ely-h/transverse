6e14bd833da8ecc61f1a63bc6bcc758e695ee867
auryo-B <129997399+auryo-B@users.noreply.github.com>
2025-05-29 21:59:25 +0200

    IOException handled

--- Transverse/src/vue/Etudiant/Liste/ListeEtudiant/PanelListeEtudiant.java
+++ Transverse/src/vue/Etudiant/Liste/ListeEtudiant/PanelListeEtudiant.java
@@ -2,6 +2,7 @@ package vue.Etudiant.Liste.ListeEtudiant;
 
 import javax.swing.*;
 import java.awt.GridLayout;
+import java.io.IOException;
 import java.util.*;
 
 import modele.Etudiant;
@@ -19,7 +20,7 @@ public class PanelListeEtudiant extends JPanel{
 	//---------------
 	//CONSTRUCTEUR
 	//---------------
-	public PanelListeEtudiant(ArrayList<Etudiant> _lE){
+	public PanelListeEtudiant(ArrayList<Etudiant> _lE) throws IOException{
 		
 		for(int i=0; i<_lE.size();i++) {
 			add(new PanelLigneEtudiant(_lE.get(i)));
