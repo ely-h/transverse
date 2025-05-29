2dad8f830626fb11ed66e81d3fbedd2891927e5b
Elyssa Hallou <elyssa.hallou@universite-paris-saclay.fr>
2025-05-29 15:16:11 +0200

    Ajout setupLayout fonction pour la mise en page

--- Transverse/src/vueMaison/PanelSelection.java
+++ Transverse/src/vueMaison/PanelSelection.java
@@ -16,6 +16,7 @@ public class PanelSelection extends JPanel{
 	JCheckBox checkbox;
 	private Maison maison;
 	
+	//Constructeur des selections de maison
 	PanelSelection(Maison maison, String cbxNom, String imgNom) throws IOException
 	{
 		BufferedImage bufferedImg = ImageIO.read(new File(imgNom + ".jpg"));
@@ -27,6 +28,8 @@ public class PanelSelection extends JPanel{
 
 		setLayout(new GridLayout(2,1));
 		this.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY,3));
+		
+		//ajout des composants
 		add(label);
 		add(checkbox);
 		this.maison = maison;
@@ -39,7 +42,8 @@ public class PanelSelection extends JPanel{
 	public void setMaison(Maison maison) {
 		this.maison = maison;
 	}
-
+	
+	//Option tout selectionner
 	public boolean toutSelection() {
         return checkbox.isSelected();
     }
@@ -47,18 +51,4 @@ public class PanelSelection extends JPanel{
     public void setDeselected(boolean deselected) {
         checkbox.setSelected(deselected);
     }
-	
-	/*public static void main(String[] args) {
-		PanelSelection vueTest;
-		try {
-			vueTest = new PanelSelection(,"Essai","img/maison-japon");
-			JFrame fenetre = new JFrame();
-			fenetre.setSize(300,500);
-			fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			fenetre.add(vueTest);
-			fenetre.setVisible(true);
-		} catch(IOException e) {
-			e.printStackTrace();
-		}
-	}*/
 }
