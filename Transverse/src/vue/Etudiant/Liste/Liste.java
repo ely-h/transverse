6562c5a828b43dde9649460fd02b2ea9d8d8198b
auryo-B <129997399+auryo-B@users.noreply.github.com>
2025-05-29 19:56:20 +0200

    GridLayout to BorderLayout

--- Transverse/src/vue/Etudiant/Liste/Liste.java
+++ Transverse/src/vue/Etudiant/Liste/Liste.java
@@ -1,10 +1,12 @@
 package vue.Etudiant.Liste;
 
 import javax.swing.*;
-import java.awt.GridLayout;
+import java.awt.BorderLayout;
 import java.util.*;
 
+import modele.FactoryCIUP;
 import modele.Maison;
+import vue.Etudiant.Liste.Filtre.PanelFiltre;
 
 public class Liste extends JPanel{
 
@@ -12,28 +14,31 @@ public class Liste extends JPanel{
 	//ATTRIBUTS
 	//---------------
 	private PanelFiltre _pFiltre;
-	
-	
-	//---------------
-	//ACCESSEUR
-	//---------------
+	private PanelLigneEtudiant _pLigneEtu;
 	
 	//---------------
 	//CONSTRUCTEUR
 	//---------------
 	Liste(ArrayList<Maison> alMaison){
 		_pFiltre = new PanelFiltre(alMaison);
-	
-		add(_pFiltre);
-		
-		setLayout(new GridLayout(1,2));
+		_pLigneEtu = new PanelLigneEtudiant("Jones", "Felicia", "Amerique", 5);
 	}
-	
 	
 	//---------------
 	//MAIN
 	//---------------
 	public static void main(String[] arg) {
+		FactoryCIUP facto = FactoryCIUP.getInstance();
+		facto.CreationObjets();
 		
+		
+		JFrame liste = new JFrame("liste");
+		liste.setSize(700, 300);
+		liste.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		Liste pl = new Liste(facto.getLesMaisons());
+		
+		liste.add(pl);
+		liste.setVisible(true);
 	}
 }
